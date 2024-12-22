b814947de068064e681201557409fc4e4e35e64a
AbdelrahmanElzayat <bodyelmajec@gmail.com>
2024-12-22 17:46:49 +0200

    bold footer name

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
     <footer className="bg-backgroundBlue flex justify-center items-center py-3">
       <div className="container">
         <p className="text-center text-textPrimary text-sm font-normal">
-          جميع الحقوق محفوظة© شركة منتجات الحيوان
+          جميع الحقوق محفوظة©{" "}
+          <span className="font-bold">شركة منتجات الحيوان</span>
         </p>
       </div>
     </footer>
