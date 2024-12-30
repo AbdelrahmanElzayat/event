9dfa460eaeb9790d3f0aa61855abc0201ec3f4a0
AbdelrahmanElzayat <bodyelmajec@gmail.com>
2024-12-30 22:34:47 +0200

    translate footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
+  const { t } = useTranslation();
   return (
-    <footer id="contact-us" className="bg-backgroundBlue flex justify-center items-center py-3">
+    <footer
+      id="contact-us"
+      className="bg-backgroundBlue flex justify-center items-center py-3"
+    >
       <div className="container">
         <p className="text-center text-textPrimary text-sm font-normal">
-          جميع الحقوق محفوظة©{" "}
-          <span className="font-bold">شركة منتجات الحيوان</span>
+          {t("allRightsReserved")}{" "}
+          <span className="font-bold">{t("companyName")}</span>
         </p>
       </div>
     </footer>
