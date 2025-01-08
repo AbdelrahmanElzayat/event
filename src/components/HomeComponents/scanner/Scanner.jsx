1861e8275c424cbc832e60e2a9c14be386019627
AbdelrahmanElzayat <bodyelmajec@gmail.com>
2025-01-08 13:47:59 +0200

    translate text

--- src/components/HomeComponents/scanner/Scanner.jsx
+++ src/components/HomeComponents/scanner/Scanner.jsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import QRCode from "react-qr-code";
 
 const Scanner = () => {
+  const { t } = useTranslation();
   const pdfPath = "/scan.pdf";
   return (
     <div className="Scanner py-4 bg-backgroundPrimary">
       <div className="container flex justify-between items-center flex-wrap">
         <div className="scannerContent flex flex-col gap-3">
-          <h4 className="font-bold text-textPrimary">مرخص من قبل :</h4>
+          <h4 className="font-bold text-textPrimary">{t("licensedBy")}</h4>
           <div className="flex flex-col justify-center">
             <span className="text-textSecondary font-semibold text-xs">
               الـــهــيــئـــة الـــعــامــــة لــلـــمــعــــارض
