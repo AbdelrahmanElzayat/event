cfd7cd36192fecff68643a3b7aaaf2b64ad4fceb
AbdelrahmanElzayat <bodyelmajec@gmail.com>
2025-02-24 12:27:28 +0200

    moved

--- src/components/HomeComponents/scanner/Scanner.jsx
+++ src/components/HomeComponents/scanner/Scanner.jsx
@@ -2,13 +2,22 @@ import Image from "next/image";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import QRCode from "react-qr-code";
-
+import scanImg from "@/assets/images/scan.png";
 const Scanner = () => {
   const { t } = useTranslation();
   const pdfPath = "/scan.pdf";
   return (
-    <div className="Scanner py-4 bg-backgroundPrimary">
-      <div className="container flex justify-between items-center flex-wrap gap-3">
+    <div className="Scanner my-4">
+      <div className="container flex items-center justify-center">
+        <div className="imgScan bg-backgroundBlue py-4 w-[250px] md:w-[600px]">
+          <Image
+            src={scanImg}
+            alt=""
+            style={{ maxHeight: "150px", objectFit: "contain" }}
+          />
+        </div>
+      </div>
+      {/* <div className="container flex justify-between items-center flex-wrap gap-3">
         <div className="scannerContent flex flex-col gap-3">
           <h4 className="font-bold text-textPrimary">{t("licensedBy")}</h4>
           <div className="flex flex-col justify-center">
@@ -34,7 +43,7 @@ const Scanner = () => {
             viewBox={`0 0 256 256`}
           />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
